870a68b7d4ed73d5c07866cb25e0a44cd99592e0
boshenz <boshenz@vt.edu>
2022-02-26 02:43:20 -0500

    working commit

---
+++ popup.js
@@ -0,0 +1,15 @@
+$('#search').on('click', () => {
+    var searchfor = $('#search-for').val();
+    console.log("searchfor: " + searchfor);
+    var format = $('#search-format').val();
+    console.log("in format: " + format);
+    if (format == "plaintext") {
+      //function for
+    }
+    else if(format =="image"){
+
+    }
+    else if(format == "video"){
+        
+    }
+  });
