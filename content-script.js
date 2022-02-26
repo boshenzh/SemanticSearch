f6248e985d1c684bf5ef6da60eb306caf44c6600
wyuwei416 <wyuwei@vt.edu>
2022-02-26 18:39:17 -0500

    not get video from content anymore

--- content-script.js
+++ content-script.js
@@ -4,8 +4,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (start) {
         const images = document.getElementsByTagName('img');
         const imgSrcList = Array.from(images).map((img) => img.src);
-        const ytPlayer = document.getElementsByClassName('video')[0];
-        const variable = [ytPlayer, imgSrcList];
-        sendResponse(variable);
+        sendResponse(imgSrcList);
     }
 });
