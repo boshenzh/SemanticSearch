319907413f3a61f344f891b45206f7c198ab7133
Yuwei <47401847+wyuwei416@users.noreply.github.com>
2022-02-27 02:26:40 -0500

    Update popup.js
    implement video search

--- popup.js
+++ popup.js
@@ -1,61 +1,33 @@
-
 let srcList;
 let r;
 let idList;
 let getContentList;
 let intervalT;
-let searchedImg;
+let intervalV;
+let searchedImg = new Array();
+let videoStatus;
 //suppose video time found is 3s,790s,5h3s
-var videoStamp = [123, 12312, 44, 123123];
+let videoStamp;
+let ytVideoUrl;
 var href = "";
 var currentSearchId = 0;
 var totalSearchnum = 0;
-let currentTab = (
-    await chrome.tabs.query({ active: true, currentWindow: true })
-)[0];
-document.getElementById("search-format").onchange = function () {
-    videoPlatformCheck();
-};
-var getLocation = function (href) {
-    var l = document.createElement("a");
-    l.href = href;
-    return l;
-};
+let currentTab = (await chrome.tabs.query({active: true, currentWindow: true}))[0];
 
-var nextButton = document.getElementById("next");
-nextButton.addEventListener("click", nextEntry);
-var prevButton = document.getElementById("prev");
-prevButton.addEventListener("click", prevEntry);
-totalSearchnum
-function nextEntry() {
-    if (currentSearchId < totalSearchnum) {
-        currentSearchId += 1;
-    }
-    
-        const displayc = currentSearchId;
-        document.getElementById("entry").innerHTML = displayc + "/" + totalSearchnum;
-
-    
-}
-function prevEntry() {
-    if (currentSearchId > 0) {
-        currentSearchId -= 1;
-    }
-        const displayc = currentSearchId+1;
-
-        if(totalSearchnum !=0){
-            document.getElementById("entry").innerHTML = displayc + "/" + totalSearchnum;
-
-        }
-
-    
-
+inject();
+document.getElementById('search-format').onchange = function() {
+  videoPlatformCheck();
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, (tabArray) => {
+chrome.tabs.query({ active: true, currentWindow: true },  (tabArray) =>{
     const url = tabArray[0].url;
+    const vUrl = url;
     href = getLocation(url).pathname + getLocation(url).search;
     const hostname = getLocation(url).hostname; // we're interested in host related data
+    if(hostname == "www.youtube.com"){
+        console.log("url: "+vUrl);
+        postVideoToServer(vUrl);
+    }
     document.getElementById("link").textContent = hostname;
 });
 
@@ -68,20 +40,20 @@ chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
         }
         srcList = Array.from(new Set(res[0]));
         idList = new Array();
-        searchedImg = new Array();
-        const imgList = srcList.map((src) => `<img src="${src}" />`).join("");
-        for (let i = 0; i < srcList.length; i++) {
+        //const imgList = srcList.map((src) => `<img src="${src}" />`).join('');
+        for(let i =0;i<srcList.length;i++){
             imageURL(srcList[i], i, "png");
         }
-        intervalT = setInterval(getContentListFromServer, 1000);
+        intervalT=setInterval(getContentListFromServer,1000);
 
         //document.getElementById('app').innerHTML = imgList;
     });
 });
 
-function videoPlatformCheck() {
-    console.log("here");
-    console.log($("#search-format").val());
+
+function videoPlatformCheck(){
+  console.log("here");
+  console.log($('#search-format').val());
 
     //get hostname information
     const url = currentTab.url;
@@ -89,70 +61,75 @@ function videoPlatformCheck() {
     const hostname = getLocation(url).hostname; // we're interested in host related data
     console.log(hostname);
 
-    if ($("#search-format").val() == "video") {
-        if (hostname == "www.youtube.com") {
-            document.getElementById("search").style.pointerEvents = "auto";
-            $("#search-for").pointerEvents("disabled", false);
-            //   $("#search").toggleClass("disabled");
-            $("#error").text("");
-            console.log("yes");
-        } else {
-            $("#search-for").prop("disabled", true);
-            //$("#search").toggleClass("disabled");
-            // var icon = document.getElementById("search");
-            //icon.disabled = true;
-            document.getElementById("search").style.pointerEvents = "none";
-            $("#error").text("this function is only available for Youtube now");
-            console.log("no");
-        }
-    } else {
-        $("#search-for").prop("disabled", false);
-        //   $("#search").prop('disabled', false);
-        //var icon = document.getElementById("search");
-        //icon.disabled = false;
+  if($('#search-format').val() =="video"){
+    if (hostname == "www.youtube.com"){
         document.getElementById("search").style.pointerEvents = "auto";
-        $("#error").text("");
-        console.log("yes");
+      $("#search-for").prop('disabled', false);
+    //   $("#search").toggleClass("disabled");
+      $("#error").text("");
+      console.log("yes");
+
     }
+    else{
+      $("#search-for").prop('disabled', true);
+      //$("#search").toggleClass("disabled");
+     // var icon = document.getElementById("search");
+      //icon.disabled = true;
+        document.getElementById("search").style.pointerEvents = "none";
+      $("#error").text("this function is only available for Youtube now");
+      console.log("no");
+
+    }
+
+  }
+  else{
+    $("#search-for").prop('disabled', false);
+    //   $("#search").prop('disabled', false);
+      //var icon = document.getElementById("search");
+      //icon.disabled = false;
+      document.getElementById("search").style.pointerEvents = "auto";
+      $("#error").text("");
+      console.log("yes");
+  }
+
 }
 
-$("#search").on("click", () => {
-    var list = document.getElementById("link-list");
+$('#search').on('click', async () => {
+    var list = document.getElementById('link-list');
     list.innerHTML = "";
-    var searchfor = $("#search-for").val();
+    var searchfor = $('#search-for').val();
     console.log("searchfor: " + searchfor);
-    var format = $("#search-format").val();
+    var format = $('#search-format').val();
     console.log("in format: " + format);
 
     if (format == "plaintext") {
         //searchText(searchfor);
     } else if (format == "image") {
-        totalSearchnum = searchedImg.length;
-    
+        const textList = await search(searchfor);
+        //console.log(textList);
+        searchImage(textList, searchedImg);
     } else if (format == "video") {
-        totalSearchnum = videoStamp.length;
-        if (videoStamp.length == 0) {
-            console.log("should load");
-            //document.getElementById("searchbox").classList.add('loading');
-            console.log(document.getElementById("search-box").className);
-            document.getElementById("search-box").className =
-                "ui left icon input loading";
-
-            // $("#search-for").prop('loading', true);
-            // $("#search").prop('disabled', true);
-        } else {
-            document.getElementById("search-box").className = "ui icon input";
-
+        if(!videoStatus.includes("Done")){
+            $("#search-for").prop('disabled', true);
+            document.getElementById("search").style.pointerEvents = "none";
+        }
+        else{
+            $("#search-for").prop('disabled', false);
+            document.getElementById("search").style.pointerEvents = "auto";
+            const textList = await search(searchfor);
+            const url = currentTab.url;
+            await searchedVideoDuration(url, textList);
+            console.log("vOnclick: "+videoStamp.length);
             for (var j = 0; j < videoStamp.length; j++) {
-                var item = document.createElement("div");
+                var item = document.createElement('div');
                 item.class = "item";
-                list = document.getElementById("link-list");
+                list = document.getElementById('link-list');
                 list.appendChild(item);
-                var content = document.createElement("div");
+                var content = document.createElement('div');
                 content.class = "content";
                 item.appendChild(content);
 
-                var aTag = document.createElement("a");
+                var aTag = document.createElement('a');
                 console.log(videoStamp[j]);
 
                 aTag.innerText = convertTimeFormat(videoStamp[j]);
@@ -161,8 +138,9 @@ $("#search").on("click", () => {
 
                 const onClick = (event) => {
                     console.log(event.target.id);
-                    chrome.tabs.sendMessage(currentTab.id, { time: event.target.id });
-                };
+                    chrome.tabs.sendMessage(currentTab.id, {time: event.target.id});
+
+                }
                 aTag.addEventListener("click", onClick);
 
                 // window.addEventListener('click', onClick);
@@ -180,14 +158,15 @@ $("#search").on("click", () => {
 });
 
 function imageURL(url, filename, fileType) {
-    getBase64(url, filename, fileType, (_baseUrl) => { });
+    getBase64(url, filename, fileType, (_baseUrl) => {
+    });
 }
 
 function getBase64(url, filename, fileType, callback) {
     var image = new Image(),
-        dataURL = "";
+        dataURL = '';
     image.src = url;
-    image.crossOrigin = "*";
+    image.crossOrigin="*";
     image.onload = function () {
         var canvas = document.createElement("canvas"),
             width = image.width,
@@ -195,53 +174,119 @@ function getBase64(url, filename, fileType, callback) {
         canvas.width = 224;
         canvas.height = 224;
         canvas.getContext("2d").drawImage(image, 0, 0, 224, 224);
-        dataURL = canvas.toDataURL("image/" + fileType);
+        dataURL = canvas.toDataURL('image/' + fileType);
         if (width < 64 || height < 64) {
             console.log("pic too small");
-        } else {
+        }
+        else {
             //console.log("id: " + filename + "; " + dataURL.split(',')[1]);
             const rnID = filename + "rn" + Math.random() * Number.MAX_VALUE;
             idList[idList.length] = rnID;
             $.ajax({
                 type: "POST",
                 url: "http://34.86.216.171/image/post",
-                data: { id: rnID, img: dataURL.split(",")[1] },
-                success: function (data) {
+                data: {"id": rnID, "img":dataURL.split(',')[1]},
+                success: function(data){
                     console.log("success post");
-                },
+                }
             });
         }
         callback ? callback(dataURL) : null;
     };
 }
-function getContentListFromServer() {
+function getContentListFromServer(){
     $.ajax({
         type: "POST",
         url: "http://34.86.216.171/image/get",
-        data: { ids: idList },
-        success: function (data) {
+        data: {"ids": idList},
+        success: function(data){
             console.log(data);
             //const obj = JSON.parse(data);
             const result = data.result;
             console.log(result);
-            if (result.length != 0) {
+            if(result.length != 0){
                 getContentList = result;
                 clearInterval(intervalT);
                 console.log("success get");
-            } else {
-                //clearInterval(intervalT);
-                console.log("waiting...");
             }
-        },
+            else{
+                //clearInterval(intervalT);
+                console.log("waiting...")
+            }
+        }
     });
 }
-function timeStampClicked() {
+function postVideoToServer(videoUrl){
+    $.ajax({
+        type: "POST",
+        url: "http://34.86.216.171/video/post",
+        data: {"url": videoUrl},
+        success: function(data){
+            ytVideoUrl = videoUrl;
+            console.log("success post video");
+            intervalV=setInterval(getDurationListFromServer,1000);
+        }
+    });
+}
+function getDurationListFromServer(){
+    $.ajax({
+        type: "POST",
+        url: "http://34.86.216.171/video/get",
+        data: {"url": ytVideoUrl},
+        success: function(data){
+            console.log(data);
+            //const obj = JSON.parse(data);
+            const status = data.status;
+            console.log(status);
+
+            if(status == "Done"){
+                clearInterval(intervalV);
+                videoStatus = "Done";
+                console.log("Done");
+            }
+            else if (status.includes("Error")){
+                clearInterval(intervalV);
+                videoStatus = "Something Error Happened";
+                console.log("Error");
+            }
+            else{
+                videoStatus = "Processing Video...";
+                console.log("Processing");
+            }
+        }
+    });
+}
+async function searchedVideoDuration(videoUrl, keywords){
+    new Promise(resolve =>{
+        $.ajax({
+            type: "POST",
+            url: "http://34.86.216.171/video/search",
+            data: {"url": videoUrl, "keywords": keywords},
+            success: function(data){
+                const times = data.times;
+                videoStamp = times;
+                console.log("success get keywords");
+                console.log("searchVideo: "+videoStamp.length);
+                resolve()
+
+            }
+        });
+    })
+
+}
+var getLocation = function (href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+function timeStampClicked(){
     console.log("clicked");
     var text;
 
     // chrome.tabs.query({active:true,currentWindow:true},()=>inject())
-    chrome.tabs.sendMessage(currentTab.id, { time: text });
+    chrome.tabs.sendMessage(currentTab.id,{time:text});
     // console.log(aid);
+
 }
 function inject(){
     chrome.scripting.executeScript(
@@ -253,7 +298,7 @@ function inject(){
 
 }
 
-function convertTimeFormat(seconds) {
+function convertTimeFormat(seconds){
     var date = new Date(null);
     date.setSeconds(seconds); // specify value for SECONDS here
     var result = date.toISOString().substr(11, 8);
@@ -261,13 +306,51 @@ function convertTimeFormat(seconds) {
 }
 
 function searchImage(text, imgResults) {
-    for (let i = 0; i < imgResults.length; i++) {
+    for (let i = 0; i < imgResults.length; i++){
         const content = imgResults[i].content;
         const id = imgResults[i].id.split("rn")[0];
-        for (let j = 0; j < text.length; j++) {
-            if (content.contains(text[j])) {
+        for(let j = 0; j< text.length;j++){
+            if(content.includes(text[j])){
+                console.log("id: " + id);
                 searchedImg[searchedImg.length] = id;
             }
         }
     }
+}
+
+const token = {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "c2704e88abmshc38f6f7d141ce05p1ee4cfjsn103a6e176ef9"
+    }
+}
+
+const methods = ["synonyms"]
+
+let getSynonyms = async function (str, method) {
+    return new Promise((resolve) => {
+        const http = new XMLHttpRequest();
+        http.addEventListener("readystatechange", function () {
+
+            if (this.readyState == this.DONE) {
+                let json = JSON.parse(this.responseText)
+                resolve(json[method])
+            }
+        })
+        http.open("GET", "https://wordsapiv1.p.rapidapi.com/words/" + str + "/" + method)
+        http.setRequestHeader("x-rapidapi-host", "wordsapiv1.p.rapidapi.com");
+        http.setRequestHeader("x-rapidapi-key", "c2704e88abmshc38f6f7d141ce05p1ee4cfjsn103a6e176ef9")
+        http.send()
+    })
+}
+
+
+async function search(str) {
+    let res = []
+    for (let i = 0; i < methods.length; i++) {
+        res = res.concat(await getSynonyms(str, methods[i]))
+    }
+    res.push(str);
+    return res
 }
